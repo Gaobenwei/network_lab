ef2b07bcba577507a46a66d7d626c7e47ebac4f2
Gaobenwei <2752528393@qq.com>
2023-07-19 19:36:50 +0000

    lab3

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-/* TCPSender constructor (uses a random ISN if none given) */
+/* TCPSender constructor (uses a random ISN if none given) TCPSender构造函数(如果没有给出则使用随机ISN)*/
 TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn )
   : isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) ), initial_RTO_ms_( initial_RTO_ms )
 {}
@@ -13,41 +13,138 @@ TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn )
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
   // Your code here.
-  return {};
+  return outstanding_count_;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
   // Your code here.
-  return {};
+  return retransmissions_count_;
 }
 
 optional<TCPSenderMessage> TCPSender::maybe_send()
 {
   // Your code here.
-  return {};
+  if(messages_.empty())
+  {
+    return {};
+  }
+  if(!timer_.is_running())
+  {
+    timer_.start();
+  }
+  TCPSenderMessage msg=messages_.front();
+  messages_.pop();
+  return msg;
+
 }
 
 void TCPSender::push( Reader& outbound_stream )
 {
-  // Your code here.
-  (void)outbound_stream;
+  // Your code here
+  //可用窗口大小内不断发送段
+  uint16_t win_now_size=win_size_==0?1:win_size_;
+  while(outstanding_count_<win_now_size)
+  {
+    TCPSenderMessage msg;
+    //起始部分发送SYN标志
+    if(syn_==false)
+    {
+      syn_=msg.SYN=true;
+      outstanding_count_+=1;
+    }
+    //段的序列号
+    msg.seqno=Wrap32::wrap(next_seqno_,isn_);
+    //段的内容string，合适的大小，从read流中产生
+    auto str_size=std::min(TCPConfig::MAX_PAYLOAD_SIZE,win_now_size-outstanding_count_);
+    read(outbound_stream,str_size,msg.payload);
+    outstanding_count_+=msg.payload.size();
+    //是否发送结束标志
+    if(!fin_&&outbound_stream.is_finished()&&outstanding_count_<win_now_size)
+    {
+      fin_=msg.FIN=true;
+      outstanding_count_+=1;
+    }
+    //空段则不用发送
+    if(msg.sequence_length()==0)
+    {
+      break;
+    }
+    //更新sender记录的信息
+    messages_.push(msg);
+    //测试是否需要，push立刻发送？
+    next_seqno_+=msg.sequence_length();
+    outstanding_segments_.push(msg);
+
+    //结束标志or流中数据无缓冲的数据
+    if(msg.FIN||outbound_stream.bytes_buffered()==0)
+    {
+      break;
+    }
+  }
 }
 
 TCPSenderMessage TCPSender::send_empty_message() const
 {
   // Your code here.
-  return {};
+  TCPSenderMessage msg;
+  msg.seqno=Wrap32::wrap(next_seqno_,isn_);
+  msg.FIN=msg.SYN=false;
+  return msg;
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
   // Your code here.
-  (void)msg;
+  win_size_=msg.window_size;
+  //win_size_=msg.window_size==0?1:msg.window_size; 有错误
+  if(msg.ackno.has_value())
+  {
+    auto new_ackno=msg.ackno.value().unwrap(isn_,next_seqno_);
+    //测试是否有用
+    if(new_ackno>next_seqno_)
+    {
+      return;
+    }
+    rece_seqno_=new_ackno;
+    while(!outstanding_segments_.empty())
+    {
+      auto& front=outstanding_segments_.front();
+      if(front.seqno.unwrap(isn_,next_seqno_)+front.sequence_length()<=rece_seqno_)
+      {
+        outstanding_count_-=front.sequence_length();
+        outstanding_segments_.pop();
+        timer_.reset_RTO();
+        if(!outstanding_segments_.empty())
+        {
+          timer_.start();
+        }
+        retransmissions_count_=0;
+      }
+      else
+      {
+        break;
+      }
+    }
+    if(outstanding_segments_.empty())
+    {
+      timer_.stop();
+    }
+  }
 }
 
 void TCPSender::tick( const size_t ms_since_last_tick )
 {
   // Your code here.
-  (void)ms_since_last_tick;
+  timer_.tick(ms_since_last_tick);
+  if(timer_.is_expired())
+  {
+    messages_.push(outstanding_segments_.front());
+    if(win_size_!=0)
+    {
+      retransmissions_count_+=1;
+      timer_.double_RTO();
+    }
+    timer_.start();
+  }
 }
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -4,31 +4,115 @@
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
 
+
+class Timer
+{
+private:
+  //初始RTO,当时RTO
+  uint64_t initial_RTO_ ;
+  uint64_t now_RTO_ ;
+  //计时
+  uint64_t time_ {0};
+  //是否运行
+  bool is_running_ {false};
+public:
+  explicit Timer(uint64_t rto):initial_RTO_(rto),now_RTO_(rto){}
+  //启动，停止
+  void start()
+  {
+    is_running_=true;
+    time_=0;
+  }
+
+  void stop()
+  {
+    is_running_=false;
+  }
+
+  //是否运行，超时
+  bool is_running()const{return is_running_;}
+  bool is_expired()const
+  {
+    return (is_running_&& (time_>=now_RTO_));
+  }
+
+  //now_RTO_ 变化
+  void double_RTO()
+  {
+    now_RTO_*=2;
+  }
+
+  void reset_RTO()
+  {
+    now_RTO_=initial_RTO_;
+  }
+
+  //计时
+  void tick(const uint64_t ms_since_last_tick)
+  {
+    if(is_running_)
+    {
+      time_+=ms_since_last_tick;
+    }
+  }
+};
+
 class TCPSender
 {
   Wrap32 isn_;
   uint64_t initial_RTO_ms_;
+  //SYN包是否已发送
+  bool syn_ {false};
+  //FIN包是否已发送
+  bool fin_ {false};
+ 
+  //连续重传计数
+  uint64_t retransmissions_count_ {0};
+  
+  //接收确认信息知道的窗口大小、确认号
+  uint16_t win_size_ {1};
+  
+  //根据ack等调整的已确认序列号 [0,1,2,......,rece_seqno_-1]已被确认，rece_seqno_还未被确认
+  uint64_t rece_seqno_ {0};
+  
+  //下一个要发送的数据索引,[0,1,2....next_seqno_-1]都已发送，next_seqno_是下一个即将发送的
+  uint64_t next_seqno_ {0};
+  
+  //已发送未确认的数据大小。sequence_numbers_in_flight() 函数需要返回这个数值
+  uint64_t outstanding_count_ {0};
+  
+  //跟踪已发送的但是没有收到确认的报文
+  std::queue<TCPSenderMessage> outstanding_segments_ {};
+  //要发送的报文段
+  std::queue<TCPSenderMessage> messages_ {};
+
+  //重传定时器的类，记录一个segment是否超时
+  Timer timer_{initial_RTO_ms_};
 
 public:
-  /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
+  /* Construct TCP sender with given default Retransmission Timeout and possible ISN 
+  用给定的默认重传超时和可能的ISN构造TCP发送方*/
   TCPSender( uint64_t initial_RTO_ms, std::optional<Wrap32> fixed_isn );
 
-  /* Push bytes from the outbound stream */
+  /* Push bytes from the outbound stream 从出站流推送字节*/
   void push( Reader& outbound_stream );
 
-  /* Send a TCPSenderMessage if needed (or empty optional otherwise) */
+  /* Send a TCPSenderMessage if needed (or empty optional otherwise) 如果需要，发送tcpsendmessage(否则为空可选)*/
   std::optional<TCPSenderMessage> maybe_send();
 
-  /* Generate an empty TCPSenderMessage */
+  /* Generate an empty TCPSenderMessage 生成空tcpsendmessage*/
   TCPSenderMessage send_empty_message() const;
 
-  /* Receive an act on a TCPReceiverMessage from the peer's receiver */
+  /* Receive an act on a TCPReceiverMessage from the peer's receiver 
+  接收对端接收方发送的TCPReceiverMessage的动作*/
   void receive( const TCPReceiverMessage& msg );
 
-  /* Time has passed by the given # of milliseconds since the last time the tick() method was called. */
+  /* Time has passed by the given # of milliseconds since the last time the tick() method was called. 
+  自上次调用tick()方法以来，时间已经过给定的毫秒数。*/
   void tick( uint64_t ms_since_last_tick );
 
-  /* Accessors for use in testing */
-  uint64_t sequence_numbers_in_flight() const;  // How many sequence numbers are outstanding?
-  uint64_t consecutive_retransmissions() const; // How many consecutive *re*transmissions have happened?
+  /* Accessors for use in testing 用于测试的访问器*/
+  uint64_t sequence_numbers_in_flight() const;  // How many sequence numbers are outstanding? 有多少序列号是未完成的?
+  uint64_t consecutive_retransmissions() const; // How many consecutive *re*transmissions have happened? 发生了多少次连续的重传?
 };
+
