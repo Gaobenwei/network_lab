76806b76abe285eb7d7b2b7a8dd445e747857cf4
Gaobenwei <2752528393@qq.com>
2023-07-25 07:57:13 +0000

    lab5

--- src/router.cc
+++ src/router.cc
@@ -11,6 +11,10 @@ using namespace std;
 // next_hop: The IP address of the next hop. Will be empty if the network is directly attached to the router (in
 //    which case, the next hop address should be the datagram's final destination).
 // interface_num: The index of the interface to send the datagram out on.
+// route_prefix:匹配数据报目的地址的“32位”IPv4地址前缀
+// prefix_length:对于这条路由是适用的，多少高阶(最高有效)的route_prefix将需要匹配数据报的目的地址的相应位?
+// next_hop:下一跳IP地址。如果网络直接连接到路由器(在这种情况下，下一跳地址应该是数据报的最终目的地)，则为空。
+// interface_num:发送数据报的接口索引。
 void Router::add_route( const uint32_t route_prefix,
                         const uint8_t prefix_length,
                         const optional<Address> next_hop,
@@ -20,10 +24,48 @@ void Router::add_route( const uint32_t route_prefix,
        << static_cast<int>( prefix_length ) << " => " << ( next_hop.has_value() ? next_hop->ip() : "(direct)" )
        << " on interface " << interface_num << "\n";
 
-  (void)route_prefix;
-  (void)prefix_length;
-  (void)next_hop;
-  (void)interface_num;
+  routing_table_.emplace_back(route_t{route_prefix,prefix_length,next_hop,interface_num});
 }
 
-void Router::route() {}
+void Router::route() 
+{
+  /*在接口之间路由报文。对于每个接口，使用maybe_receive()方法来使用每个传入的数据报，
+  并将其在其中一个接口上发送到正确的下一跳。
+  路由器根据与数据报目的地址匹配的前缀长度最长的路由选择出接口和下一跳。*/
+  for(auto &net_interface:interfaces_)
+  {
+    auto maybe_datagram=net_interface.maybe_receive(); 
+    if(maybe_datagram.has_value())
+    {
+      InternetDatagram datagram=maybe_datagram.value();
+      auto dst_ip_addr=datagram.header.dst;
+      //寻找最长前缀匹配的路由,先将最长标记置为结尾
+      auto longest_match=routing_table_.end();
+      for(auto iter=routing_table_.begin();iter!=routing_table_.end();++iter)
+      {
+        if(iter->prefix_length_==0||(iter->route_prefix_ ^ dst_ip_addr) >> (32 - iter->prefix_length_) ==0)
+        {
+          //更新最长前缀匹配路由
+          if(longest_match==routing_table_.end()||iter->prefix_length_>longest_match->prefix_length_)
+          {
+            longest_match=iter;
+          }
+        }
+      }
+
+      //检查传入数据报的合法性，主要是检查TTL是否为0
+      auto &ttll=datagram.header.ttl;
+      if(longest_match!=routing_table_.end()&&ttll-- > 1)
+      {
+        //更新数据报的校验和,修改发送的地址
+        datagram.header.compute_checksum();
+        const auto &next_hop_addr=longest_match->next_hop_.has_value()?longest_match->next_hop_.value():Address::from_ipv4_numeric(dst_ip_addr);
+        //发送数据报
+        auto &outbound_interface=interface(longest_match->interface_num_);
+        outbound_interface.send_datagram(datagram,next_hop_addr);
+      }
+
+    }
+  }
+}
+
--- src/router.hh
+++ src/router.hh
@@ -10,6 +10,10 @@
 // immediately (from the `recv_frame` method), it stores them for
 // later retrieval. Otherwise, behaves identically to the underlying
 // implementation of NetworkInterface.
+/* NetworkInterface的包装器，使主机端接口异步:
+不是立即返回接收到的数据报(从' recv_frame '方法)，
+而是存储它们以供以后检索。
+否则，其行为与NetworkInterface的底层实现相同。*/
 class AsyncNetworkInterface : public NetworkInterface
 {
   std::queue<InternetDatagram> datagrams_in_ {};
@@ -17,16 +21,23 @@ class AsyncNetworkInterface : public NetworkInterface
 public:
   using NetworkInterface::NetworkInterface;
 
-  // Construct from a NetworkInterface
+  // Construct from a NetworkInterface 从网络接口构造
   explicit AsyncNetworkInterface( NetworkInterface&& interface ) : NetworkInterface( interface ) {}
 
-  // \brief Receives and Ethernet frame and responds appropriately.
+  // \brief Receives and Ethernet frame and responds appropriately. 接收一个以太网帧并做出相应的响应。
 
   // - If type is IPv4, pushes to the `datagrams_out` queue for later retrieval by the owner.
   // - If type is ARP request, learn a mapping from the "sender" fields, and send an ARP reply.
   // - If type is ARP reply, learn a mapping from the "target" fields.
   //
   // \param[in] frame the incoming Ethernet frame
+
+  // -如果type为IPv4，则将数据推送到' datagrams_out '队列，供所有者稍后检索。
+  // -如果类型为ARP请求，从“sender”字段中学习映射，然后发送ARP应答。
+  // -如果type为ARP reply，从target字段学习映射。
+  //
+  // \param[in] frame传入的以太网帧
+
   void recv_frame( const EthernetFrame& frame )
   {
     auto optional_dgram = NetworkInterface::recv_frame( frame );
@@ -35,7 +46,7 @@ public:
     }
   };
 
-  // Access queue of Internet datagrams that have been received
+  // Access queue of Internet datagrams that have been received //接收到的Internet数据报的访问队列
   std::optional<InternetDatagram> maybe_receive()
   {
     if ( datagrams_in_.empty() ) {
@@ -50,25 +61,38 @@ public:
 
 // A router that has multiple network interfaces and
 // performs longest-prefix-match routing between them.
+//有多个网络接口的路由器
+//在它们之间执行最长前缀匹配路由。
 class Router
 {
-  // The router's collection of network interfaces
+  // The router's collection of network interfaces 路由器的网络接口集合
   std::vector<AsyncNetworkInterface> interfaces_ {};
 
+  using route_t=struct 
+  {
+    /* data */
+    uint32_t route_prefix_;
+    uint8_t prefix_length_;
+    std::optional<Address> next_hop_;
+    size_t interface_num_;
+  };
+
+  std::list<route_t> routing_table_ {}; //路由表  
 public:
   // Add an interface to the router
   // interface: an already-constructed network interface
   // returns the index of the interface after it has been added to the router
+  // 将接口添加到路由器接口：已构建的网络接口在添加到路由器后，返回接口的索引
   size_t add_interface( AsyncNetworkInterface&& interface )
   {
     interfaces_.push_back( std::move( interface ) );
     return interfaces_.size() - 1;
   }
 
-  // Access an interface by index
+  // Access an interface by index 通过索引访问接口
   AsyncNetworkInterface& interface( size_t N ) { return interfaces_.at( N ); }
 
-  // Add a route (a forwarding rule)
+  // Add a route (a forwarding rule) 添加路由(转发规则)
   void add_route( uint32_t route_prefix,
                   uint8_t prefix_length,
                   std::optional<Address> next_hop,
@@ -80,5 +104,8 @@ public:
   // chooses the outbound interface and next-hop as specified by the
   // route with the longest prefix_length that matches the datagram's
   // destination address.
+  /*在接口之间路由报文。对于每个接口，使用maybe_receive()方法来使用每个传入的数据报，
+  并将其在其中一个接口上发送到正确的下一跳。
+  路由器根据与数据报目的地址匹配的前缀长度最长的路由选择出接口和下一跳。*/
   void route();
 };
